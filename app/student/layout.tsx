619030a2e863553645fdb5c60c418e51f66680ff
ritwickbanerjee <ritwick.banerjee@heritageit.edu>
2025-12-11 11:41:05 +0530

    Update layout.tsx

--- app/student/layout.tsx
+++ app/student/layout.tsx
@@ -9,7 +9,7 @@ export default function StudentLayout({
                 {children}
             </div>
             <footer className="py-4 text-center bg-[#0a0f1a] text-gray-500 border-t border-white/5 mt-auto z-10 relative">
-                <p className="text-[10px]">&copy; 2025 copyright Dept. of Mathematics, HIT</p>
+                <p className="text-[10px]">&copy; 2025 Dept. of Mathematics, HIT</p>
             </footer>
         </div>
     );
